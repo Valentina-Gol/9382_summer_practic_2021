d1d307e9995a29d7983322c812438fb111eeb849
Muirehe <54913485+SonKhwa@users.noreply.github.com>
2021-07-06 14:37:50 +0300

    fixed main

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/main/java/GUI/Main.java
 package GUI;
 
 public class Main {
@@ -7,13 +6,3 @@ public class Main {
         app.setVisible(true);
     }
 }
-=======
-package GUI;
- 
-public class Main {
-    public static void main(String[] args){
-        SimpleGui app = new SimpleGui();
-        app.setVisible(true);
-    }
-}
->>>>>>> cb50fc1bd4f23dbb0e0c59c454c70a0a725a7fb8:src/GUI/Main.java
