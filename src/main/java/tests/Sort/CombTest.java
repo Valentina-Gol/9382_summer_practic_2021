d49b1ef850c422728c100189d646acbaa2aca310
Valentina-Gol <vip.valya.golubeva@mail.ru>
2021-07-07 01:30:08 +0300

    fixed sortTest, some fixed in report

--- src/main/java/tests/Sort/CombTest.java
+++ src/main/java/tests/Sort/CombTest.java
@@ -1,75 +1,28 @@
-package tests.Sort;
-
+package Sort;
 
 import org.junit.Assert;
 import org.junit.Test;
 import logic.Sort.Comb;
 
-import java.util.Arrays;
-
-
-public class CombTest {
+public class CombTest extends SortTest {
 
     @Test
     public void combSortAscending() {
-        int[] arr1 = new int[0];
-        int[] arr2 = new int[10];
-        int[] arr3 = new int[100];
-        for (int i = 0; i < 100; i++){
-            if (i < 10){
-                arr2[i] = (int)(Math.random()*2000) - 1000;
-            }
-            arr3[i] = (int)(Math.random()*2000) - 1000;
-        }
-        int[] arr1ResAuto = arr1.clone();
-        int[] arr2ResAuto = arr2.clone();
-        int[] arr3ResAuto = arr3.clone();
-        Arrays.sort(arr1ResAuto);
-        Arrays.sort(arr2ResAuto);
-        Arrays.sort(arr3ResAuto);
-
-        int[] arr1ResProg = new Comb().sortAscending(arr1);
-        int[] arr2ResProg = new Comb().sortAscending(arr2);
-        int[] arr3ResProg = new Comb().sortAscending(arr3);
-        Assert.assertArrayEquals(arr1ResAuto, arr1ResProg);
-        Assert.assertArrayEquals(arr2ResAuto, arr2ResProg);
-        Assert.assertArrayEquals(arr3ResAuto, arr3ResProg);
+        int[] arr1ResProg = new Comb().sortAscending(testArr1);
+        int[] arr2ResProg = new Comb().sortAscending(testArr2);
+        int[] arr3ResProg = new Comb().sortAscending(testArr3);
+        Assert.assertArrayEquals(resArr1, arr1ResProg);
+        Assert.assertArrayEquals(resAsArr2, arr2ResProg);
+        Assert.assertArrayEquals(resAsArr3, arr3ResProg);
     }
 
     @Test
     public void combSortDescending() {
-        int[] arr1 = new int[0];
-        int[] arr2 = new int[10];
-        int[] arr3 = new int[100];
-        for (int i = 0; i < 100; i++){
-            if (i < 10){
-                arr2[i] = (int)(Math.random()*2000) - 1000;
-            }
-            arr3[i] = (int)(Math.random()*2000) - 1000;
-        }
-        int[] arr1ResAuto = arr1.clone();
-        int[] arr2ResAuto = arr2.clone();
-        int[] arr3ResAuto = arr3.clone();
-        Arrays.sort(arr1ResAuto);
-        Arrays.sort(arr2ResAuto);
-        Arrays.sort(arr3ResAuto);
-        int arr2Len = arr2ResAuto.length;
-        for (int i = 0; i < arr2Len / 2; i++){
-            int current = arr2ResAuto[i];
-            arr2ResAuto[i] = arr2ResAuto[arr2Len - i - 1];
-            arr2ResAuto[arr2Len - i - 1] = current;
-        }
-        int arr3Len = arr3ResAuto.length;
-        for (int i = 0; i < arr3Len / 2; i++){
-            int current = arr3ResAuto[i];
-            arr3ResAuto[i] = arr3ResAuto[arr3Len - i - 1];
-            arr3ResAuto[arr3Len - i - 1] = current;
-        }
-        int[] arr1ResProg = new Comb().sortDescending(arr1);
-        int[] arr2ResProg = new Comb().sortDescending(arr2);
-        int[] arr3ResProg = new Comb().sortDescending(arr3);
-        Assert.assertArrayEquals(arr1ResAuto, arr1ResProg);
-        Assert.assertArrayEquals(arr2ResAuto, arr2ResProg);
-        Assert.assertArrayEquals(arr3ResAuto, arr3ResProg);
+        int[] arr1ResProg = new Comb().sortDescending(testArr1);
+        int[] arr2ResProg = new Comb().sortDescending(testArr2);
+        int[] arr3ResProg = new Comb().sortDescending(testArr3);
+        Assert.assertArrayEquals(resArr1, arr1ResProg);
+        Assert.assertArrayEquals(resDesArr2, arr2ResProg);
+        Assert.assertArrayEquals(resDesArr3, arr3ResProg);
     }
 }
--- src/main/java/tests/Sort/ShakerTest.java
+++ src/main/java/tests/Sort/ShakerTest.java
@@ -1,73 +1,28 @@
-package tests.Sort;
+package Sort;
 
 import org.junit.Assert;
 import org.junit.Test;
 import logic.Sort.Shaker;
 
-import java.util.Arrays;
-
-public class ShakerTest {
+public class ShakerTest extends SortTest {
 
     @Test
-    public void shakerSortAscending() {
-        int[] arr1 = new int[0];
-        int[] arr2 = new int[10];
-        int[] arr3 = new int[100];
-        for (int i = 0; i < 100; i++){
-            if (i < 10){
-                arr2[i] = (int)(Math.random()*2000) - 1000;
-            }
-            arr3[i] = (int)(Math.random()*2000) - 1000;
-        }
-        int[] arr1ResAuto = arr1.clone();
-        int[] arr2ResAuto = arr2.clone();
-        int[] arr3ResAuto = arr3.clone();
-        Arrays.sort(arr1ResAuto);
-        Arrays.sort(arr2ResAuto);
-        Arrays.sort(arr3ResAuto);
-
-        int[] arr1ResProg = new Shaker().sortAscending(arr1);
-        int[] arr2ResProg = new Shaker().sortAscending(arr2);
-        int[] arr3ResProg = new Shaker().sortAscending(arr3);
-        Assert.assertArrayEquals(arr1ResAuto, arr1ResProg);
-        Assert.assertArrayEquals(arr2ResAuto, arr2ResProg);
-        Assert.assertArrayEquals(arr3ResAuto, arr3ResProg);
+    public void ShakerSortAscending() {
+        int[] arr1ResProg = new Shaker().sortAscending(testArr1);
+        int[] arr2ResProg = new Shaker().sortAscending(testArr2);
+        int[] arr3ResProg = new Shaker().sortAscending(testArr3);
+        Assert.assertArrayEquals(resArr1, arr1ResProg);
+        Assert.assertArrayEquals(resAsArr2, arr2ResProg);
+        Assert.assertArrayEquals(resAsArr3, arr3ResProg);
     }
 
     @Test
-    public void shakerSortDescending() {
-        int[] arr1 = new int[0];
-        int[] arr2 = new int[10];
-        int[] arr3 = new int[100];
-        for (int i = 0; i < 100; i++){
-            if (i < 10){
-                arr2[i] = (int)(Math.random()*2000) - 1000;
-            }
-            arr3[i] = (int)(Math.random()*2000) - 1000;
-        }
-        int[] arr1ResAuto = arr1.clone();
-        int[] arr2ResAuto = arr2.clone();
-        int[] arr3ResAuto = arr3.clone();
-        Arrays.sort(arr1ResAuto);
-        Arrays.sort(arr2ResAuto);
-        Arrays.sort(arr3ResAuto);
-        int arr2Len = arr2ResAuto.length;
-        for (int i = 0; i < arr2Len / 2; i++){
-            int current = arr2ResAuto[i];
-            arr2ResAuto[i] = arr2ResAuto[arr2Len - i - 1];
-            arr2ResAuto[arr2Len - i - 1] = current;
-        }
-        int arr3Len = arr3ResAuto.length;
-        for (int i = 0; i < arr3Len / 2; i++){
-            int current = arr3ResAuto[i];
-            arr3ResAuto[i] = arr3ResAuto[arr3Len - i - 1];
-            arr3ResAuto[arr3Len - i - 1] = current;
-        }
-        int[] arr1ResProg = new Shaker().sortDescending(arr1);
-        int[] arr2ResProg = new Shaker().sortDescending(arr2);
-        int[] arr3ResProg = new Shaker().sortDescending(arr3);
-        Assert.assertArrayEquals(arr1ResAuto, arr1ResProg);
-        Assert.assertArrayEquals(arr2ResAuto, arr2ResProg);
-        Assert.assertArrayEquals(arr3ResAuto, arr3ResProg);
+    public void ShakerSortDescending() {
+        int[] arr1ResProg = new Shaker().sortDescending(testArr1);
+        int[] arr2ResProg = new Shaker().sortDescending(testArr2);
+        int[] arr3ResProg = new Shaker().sortDescending(testArr3);
+        Assert.assertArrayEquals(resArr1, arr1ResProg);
+        Assert.assertArrayEquals(resDesArr2, arr2ResProg);
+        Assert.assertArrayEquals(resDesArr3, arr3ResProg);
     }
 }
