1a1111a88f53594293f7d8a84c41a88e9ca76a90
SonKhwa <54913485+SonKhwa@users.noreply.github.com>
2021-07-09 23:50:59 +0300

    Revert "added uneabled method"
    This reverts commit a16b714137026816a4b910ec74c829c4f8e09031.

--- src/main/java/GUI/SimpleGui.java
+++ src/main/java/GUI/SimpleGui.java
@@ -165,13 +165,6 @@ public class SimpleGui extends javax.swing.JFrame {
 
     }
 
-    public void setEnabledLeftPanel(boolean flag) {
-        leftPanel.setBackground(Color.LIGHT_GRAY);
-        for (var i = 0; i < leftPanel.getComponentCount();i++) {
-            leftPanel.getComponent(i).setEnabled(flag);
-        }
-    }
-
     public void showSort() {
         Sort workpan = DataClass.getInstance().sort;
         var layConstraints = getGridBagConstraints(1, 0, 1);
@@ -186,7 +179,6 @@ public class SimpleGui extends javax.swing.JFrame {
             try {
                 if (!(sources.sort == null || sources.mainArray == null)) {
                     showSort();
-                    setEnabledLeftPanel(false);
                 }
             } catch (Exception e) {
                 TheBestLogger.getInstance().logInfo("Nothing entered");
